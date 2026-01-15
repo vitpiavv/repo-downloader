c33ce62d598c8541c3650fd2fac44fc54d2614fa
vitpiavv <vitpia03@gmail.com>
2026-01-15 18:52:23 +0000

    w goated

---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+from pathlib import Path
+
+file_path = Path(__file__).parent / 'repos.txt'
+
+with open('repos.txt')as f: 
+    repos = [line.strip() for line in f if line.strip()]
+
+
+def ensure_git(repos):
+    if not repos.endswith('.git'):
+        return repos +'.git'
+    return repos
+
+
+for url in repos:
+    try:
+        subprocess.run(['git', 'clone', url])
+
+    except Exception as e: 
+        print(f'Failed to clone {url} {e}')
+        continue
+
+
+
+
